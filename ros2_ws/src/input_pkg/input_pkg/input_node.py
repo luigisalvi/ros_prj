5631f352ff47179653714e540ff2dc7fa0255d21
fenix-hub <n.santilio97@gmail.com>
2024-11-20 08:36:30 +0100

    Update Dockerfile and README for ROS Jazzy, remove unused bringup_pkg files

--- ros2_ws/src/input_pkg/input_pkg/input_node.py
+++ ros2_ws/src/input_pkg/input_pkg/input_node.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import rclpy  # type: ignore
 from rclpy.node import Node  # type: ignore
 from std_msgs.msg import Float32  # type: ignore
@@ -6,18 +8,21 @@ class InputNode(Node):
     def __init__(self):
         super().__init__('input_node')
         self.publisher_ = self.create_publisher(Float32, 'setpoint', 10)
-        
-        self.timer = self.create_timer(2.0, self.publish_setpoint)
+        self.get_logger().info(f'Starting listening for setpoint values...')
+
+        self.publish_setpoint()
+        #self.timer = self.create_timer(2.0, self.publish_setpoint)
         
     def publish_setpoint(self):
-        try:
-            setpoint_value = float(input("Enter the setpoint value: "))
-            msg = Float32()
-            msg.data = setpoint_value
-            self.publisher_.publish(msg)
-            self.get_logger().info(f"Published setpoint: {setpoint_value}")
-        except ValueError:
-            self.get_logger().error("Invalid input. Please enter a numeric value.")
+        while rclpy.ok():
+            try:
+                setpoint_value = float(input("Enter new setpoint:"))
+                msg = Float32()
+                msg.data = setpoint_value
+                self.publisher_.publish(msg)
+                self.get_logger().info(f"Published setpoint: {setpoint_value}")
+            except ValueError:
+                self.get_logger().error("Invalid input. Please enter a numeric value.")
 
 def main(args=None):
     rclpy.init(args=args)
