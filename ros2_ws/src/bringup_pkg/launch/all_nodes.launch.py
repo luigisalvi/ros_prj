e3d5cef16fbda960c6bf23f948e57f5875733593
luigisalvi <luigisalvi1997@libero.it>
2024-11-21 10:53:12 +0100

    commit 5 (2 containers create)

--- ros2_ws/src/bringup_pkg/launch/all_nodes.launch.py
+++ ros2_ws/src/bringup_pkg/launch/all_nodes.launch.py
@@ -19,13 +19,13 @@ def generate_launch_description():
         name='visualizer'
     )
 
-    input_node = Node(
-        package='input_pkg',
-        executable='input_node',
-        name='input'
-    )
+    # input_node = Node(
+    #     package='input_pkg',
+    #     executable='input_node',
+    #     name='input'
+    # )
     ld.add_action(data_generator_node)
     ld.add_action(pid_controller_node)
     ld.add_action(visualizer_node)
-    ld.add_action(input_node)
+    # ld.add_action(input_node)
     return ld
