f0decbbedef4f58b75e413b32c0b883214376a2b
fenix-hub <nicolo.santilio@outlook.com>
2025-05-06 23:38:48 +0200

    add foxy

---
+++ ros_entrypoint_deepracer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11:{$GAZEBO_RESOURCE_PATH}
+export GAZEBO_MODEL_PATH=/app/deepracer_nav2_ws/aws-deepracer/deepracer_gazebo/models 
+export GAZEBO_PLUGIN_PATH=/app/deepracer_nav2_ws/aws-deepracer/deepracer_gazebo/build 
+
+source /app/deepracer_nav2_ws/aws-deepracer/install/setup.bash
+source /opt/ros/foxy/setup.bash
+
+exec "$@"
